123cdcf8f47f8225246868fcd5c4b68df6cd4e91
chengr28 <chengr28@gmail.com>
2014-01-06 20:03:30 +0800

    v0.1 Beta

---
+++ Release_x86/ServiceInstall.bat
@@ -0,0 +1,12 @@
+@echo off
+sc stop PcapDNSProxyService
+sc delete PcapDNSProxyService
+sc create PcapDNSProxyService binPath= "%~dp0Pcap_DNSProxy.exe" start= auto
+sc description PcapDNSProxyService "A local DNS server program base on WinPcap."
+reg add HKLM\SYSTEM\CurrentControlSet\Services\PcapDNSProxyService\Parameters /v Application /d "%~dp0Pcap_DNSProxy.exe" /f
+reg add HKLM\SYSTEM\CurrentControlSet\Services\PcapDNSProxyService\Parameters /v AppDirectory /d "%~dp0" /f
+start Pcap_DNSProxy
+@echo.
+@echo Done. Please confirm the PcapDNSProxyService service had been installed.
+@echo.
+@pause
